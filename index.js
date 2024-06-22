369a2c45b5eb5b3a8bf190465bcdef6241c50228
Derifarhan <121393517+Derifarhan@users.noreply.github.com>
2024-06-22 11:47:44 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+import Jumbotron from "./Jumbotron";
+import Footer from "./Footer";
+import LayananKami from "./LayananKami";
+import CariKonsultasi from "./CariKonsultasi";
+import GabungSekarang from "./GabungSekarang";
+import LihatArtikel from "./LihatArtikel";
+import Beranda from "./Beranda";
+import DashboardPage from "./DashboardPage";
+import Artikel from "./Artikel";
+import Login from "./Login";
+import Daftar from "./Daftar";
+import LupaSandi from "./LupaSandi";
+import VerifikasiEmail from "./VerifikasiEmail";
+import KataSandiBaru from "./KataSandiBaru";
+import NotFound from "./NotFound";
+import Konsultasi from "./Konsultasi";
+import Payment from "./Payment";
+import ChatKonsultan from "./ChatKonsultasi.jsx";
+import Profile from "./Profile";
+import Schedule from "./Schedule";
+import TukarPoint from "./TukarPoint.jsx";
+import Penjadwalan from "./Penjadwalan.jsx";
+import Transaksi from "./Transaksi.jsx";
+
+export {
+  Beranda,
+  DashboardPage,
+  Artikel,
+  Jumbotron,
+  LayananKami,
+  CariKonsultasi,
+  GabungSekarang,
+  LihatArtikel,
+  Footer,
+  Login,
+  Daftar,
+  LupaSandi,
+  VerifikasiEmail,
+  KataSandiBaru,
+  NotFound,
+  Konsultasi,
+  Payment,
+  ChatKonsultan,
+  Profile,
+  Schedule,
+  TukarPoint,
+  Penjadwalan,
+  Transaksi,
+};
