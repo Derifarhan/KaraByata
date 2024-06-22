543a4df3e2ce59b32bd6b02457c3ee99cf844e7e
Derifarhan <121393517+Derifarhan@users.noreply.github.com>
2024-06-22 13:29:46 +0700

    Add files via upload

--- backend/controllers/Users.js
+++ backend/controllers/Users.js
@@ -1,9 +1,7 @@
-import Users from '../models/UserModel.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 import path from 'path'
 import fs from 'fs'
-import jwt from 'jsonwebtoken'
-import nodemailer from 'nodemailer'
 import User from '../models/UserModel.js'
 
 const SECRET_KEY =
@@ -11,43 +9,45 @@ const SECRET_KEY =
 
 export const getUsers = async (req, res) => {
   try {
-    const users = await Users.findAll()
+    const users = await User.findAll()
     res.json(users)
   } catch (error) {
     console.log(error)
+    res.status(500).json({ msg: 'Internal server error' })
   }
 }
 
 export const Register = async (req, res) => {
   const { username, email, password } = req.body
 
-  const user = await Users.findOne({ where: { username } })
-  if (user) {
-    return res.status(404).json({ msg: 'Username sudah ada! ganti username!' })
-  }
-
-  const salt = await bcrypt.genSalt()
-  const hashPassword = await bcrypt.hash(password, salt)
   try {
-    await Users.create({
-      username: username,
-      email: email,
+    const user = await User.findOne({ key: 'username', value: username })
+    if (user) {
+      return res
+        .status(404)
+        .json({ msg: 'Username sudah ada! ganti username!' })
+    }
+
+    const salt = await bcrypt.genSalt()
+    const hashPassword = await bcrypt.hash(password, salt)
+
+    await User.create({
+      username,
+      email,
       password: hashPassword,
       role: 'user',
     })
+
     res.json({ msg: 'Register Berhasil' })
   } catch (error) {
     console.log(error)
+    res.status(500).json({ msg: 'Internal server error' })
   }
 }
 
 export const Login = async (req, res) => {
   try {
-    const user = await Users.findOne({
-      where: {
-        email: req.body.email,
-      },
-    })
+    const user = await User.findOne({ key: 'email', value: req.body.email })
 
     if (!user) {
       return res.status(404).json({ msg: 'User not found' })
@@ -77,21 +77,22 @@ export const Login = async (req, res) => {
       },
     )
 
-    await Users.update(
-      { refresh_token: accessToken },
-      {
-        where: {
-          id,
-        },
-      },
-    )
+    await User.update(id, {
+      refresh_token: accessToken,
+      name: user.name || null,
+      username: user.username || null,
+      email: user.email || null,
+      password: user.password || null,
+      alamat: user.alamat || null,
+      phone: user.phone || null,
+      image: user.image || null,
+      url: user.url || null,
+    })
 
     res.json({ accessToken, role })
   } catch (error) {
     console.log(error.message)
-    return res.status(401).json({
-      msg: error.message,
-    })
+    res.status(500).json({ msg: 'Internal server error' })
   }
 }
 
@@ -99,28 +100,23 @@ export const Me = async (req, res) => {
   try {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    if (!token) return res.status(401).json({ msg: 'Unauthorized' }) // Unauthorized if no token provided
+    if (!token) return res.status(401).json({ msg: 'Unauthorized 1' })
 
-    const decoded = jwt.verify(
-      token,
-      process.env.ACCESS_TOKEN_SECRET || 'qwerttyuio12345asdfghjkl67890zxcvbnm',
-    )
+    const decoded = jwt.verify(token, SECRET_KEY)
 
-    const user = await Users.findOne({
-      where: {
-        id: decoded.userId,
-        refresh_token: token, // Verify token existence in the database
-      },
+    const user = await User.findOne({
+      key: 'id',
+      value: decoded.userId,
     })
 
-    if (!user) {
-      return res.status(401).json({ msg: 'Unauthorized' }) // Unauthorized if token not found in database
-    }
+    // if (!user || user.refresh_token !== token) {
+    //   return res.status(401).json({ msg: 'Unauthorized 2' })
+    // }
 
-    res.json(user) // Return the user details
+    res.json(user)
   } catch (error) {
     console.log(error.message)
-    res.status(500).send('Internal Server Error')
+    res.status(500).send('Internal server error')
   }
 }
 
@@ -128,33 +124,22 @@ export const Logout = async (req, res) => {
   try {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    if (!token) return res.status(204).json({ msg: 'No token provided' }) // No content if no token
+    if (!token) return res.status(204).json({ msg: 'No token provided' })
 
-    const user = await Users.findOne({
-      where: {
-        refresh_token: token,
-      },
-    })
+    const user = await User.findOne({ key: 'refresh_token', value: token })
 
     if (!user) {
       return res
         .status(204)
-        .json({ msg: 'User not found or already logged out' }) // No content if user not found
+        .json({ msg: 'User not found or already logged out' })
     }
 
-    await Users.update(
-      { refresh_token: null },
-      {
-        where: {
-          id: user.id,
-        },
-      },
-    )
+    await User.update(user.id, { refresh_token: null })
 
-    return res.status(200).json({ msg: 'Logout successful' })
+    res.status(200).json({ msg: 'Logout successful' })
   } catch (error) {
     console.log('Logout error:', error.message)
-    return res.status(500).json({ msg: 'Internal server error' })
+    res.status(500).json({ msg: 'Internal server error' })
   }
 }
 
@@ -162,31 +147,19 @@ export const updateUsersWithToken = async (req, res) => {
   try {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    if (!token) return res.status(401).json({ msg: 'Unauthorized' }) // Unauthorized if no token provided
+    if (!token) return res.status(401).json({ msg: 'Unauthorized' })
 
-    const decoded = jwt.verify(
-      token,
-      process.env.ACCESS_TOKEN_SECRET || 'qwerttyuio12345asdfghjkl67890zxcvbnm',
-    )
+    const decoded = jwt.verify(token, SECRET_KEY)
 
-    const user = await Users.findOne({
-      where: {
-        id: decoded.userId,
-        refresh_token: token, // Verify token existence in the database
-      },
-    })
+    const user = await User.findOne({ key: 'id', value: decoded.userId })
 
-    if (!user) {
-      return res.status(401).json({ msg: 'Unauthorized' }) // Unauthorized if token not found in database
+    if (!user || user.refresh_token !== token) {
+      return res.status(401).json({ msg: 'Unauthorized' })
     }
 
     let fileName = user.image || 'default.png'
     if (req.files && req.files.image) {
-      // Periksa req.files.image
-      console.log('File upload detected')
-
-      const file = req.files.image // Akses file sebagai req.files.image
-      const fileSize = file.size
+      const file = req.files.image
       const ext = path.extname(file.name)
       fileName = file.md5 + ext
 
@@ -194,7 +167,7 @@ export const updateUsersWithToken = async (req, res) => {
       if (!allowedType.includes(ext.toLowerCase())) {
         return res.status(422).json({ msg: 'Invalid image type' })
       }
-      if (fileSize > 5000000) {
+      if (file.size > 5000000) {
         return res.status(422).json({ msg: 'Max image size is 5MB' })
       }
 
@@ -203,38 +176,51 @@ export const updateUsersWithToken = async (req, res) => {
           console.log('Error moving file:', err.message)
           return res.status(500).json({ msg: err.message })
         }
-        console.log('File uploaded successfully')
       })
-    } else {
-      console.log('No file upload detected')
     }
 
     const url = `${req.protocol}://${req.get('host')}/images/${fileName}`
 
     const { name, email, phone, alamat } = req.body
 
-    await Users.update(
-      {
-        name,
-        username: user.username,
-        email,
-        phone,
-        alamat,
-        role: 'user',
-        image: fileName,
-        url: url,
-      },
-      {
-        where: {
-          id: user.id,
-        },
-      },
-    )
+    console.log('alamat', alamat)
+
+    await User.update(user.id, {
+      name: name || null,
+      username: user.username || null,
+      email: email || null,
+      phone: phone || null,
+      alamat: alamat || null,
+      password: user.password || null,
+      role: 'user',
+      image: fileName || 'default.png',
+      url: url || null,
+    })
 
     res.status(200).json({ msg: 'User updated successfully' })
   } catch (error) {
     console.log('Error updating user:', error.message)
-    res.status(500).json({ msg: error.message, error: 'Internal server error' })
+    res.status(500).json({ msg: 'Internal server error' })
+  }
+}
+
+export const updatepassword = async (req, res) => {
+  try {
+    const user = await User.findOne({ key: 'email', value: req.body.email })
+
+    const { password } = req.body
+
+    const salt = await bcrypt.genSalt()
+    const hashPassword = await bcrypt.hash(password, salt)
+
+    await User.updatepassword(user.id, {
+      password: hashPassword || null,
+    })
+
+    res.status(200).json({ msg: 'User updated successfully' })
+  } catch (error) {
+    console.log('Error updating user:', error.message)
+    res.status(500).json({ msg: 'Internal server error' })
   }
 }
 
@@ -242,22 +228,16 @@ export const deleteUser = async (req, res) => {
   try {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    if (!token) return res.status(401).json({ msg: 'Unauthorized' }) // Unauthorized if no token provided
+    if (!token) return res.status(401).json({ msg: 'Unauthorized' })
 
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    const decoded = jwt.verify(token, SECRET_KEY)
 
-    const user = await Users.findOne({
-      where: {
-        id: decoded.userId,
-        refresh_token: token, // Verify token existence in the database
-      },
-    })
+    const user = await User.findOne({ key: 'id', value: decoded.userId })
 
-    if (!user) {
-      return res.status(401).json({ msg: 'Unauthorized' }) // Unauthorized if token not found in database
+    if (!user || user.refresh_token !== token) {
+      return res.status(401).json({ msg: 'Unauthorized' })
     }
 
-    // Optionally remove the user's image
     if (user.image && user.image !== 'default.png') {
       const filePath = `./public/images/${user.image}`
       fs.unlink(filePath, (err) => {
@@ -267,15 +247,11 @@ export const deleteUser = async (req, res) => {
       })
     }
 
-    await Users.destroy({
-      where: {
-        id: user.id,
-      },
-    })
+    await User.delete(user.id)
 
     res.status(200).json({ msg: 'User deleted successfully' })
   } catch (error) {
     console.log(error.message)
-    res.status(500).json({ msg: error.message })
+    res.status(500).json({ msg: 'Internal server error' })
   }
 }
