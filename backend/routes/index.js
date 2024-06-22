3a7042963d9982aedb14d97891264812ef32b4db
Derifarhan <121393517+Derifarhan@users.noreply.github.com>
2024-06-22 13:27:42 +0700

    Add files via upload

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,24 +1,24 @@
 import express from 'express'
 import {
   getUsers,
-  Login,
-  Logout,
-  Me,
   Register,
-  deleteUser,
+  Login,
+  Me,
+  Logout,
   updateUsersWithToken,
+  deleteUser,
+  updatepassword,
 } from '../controllers/Users.js'
-import { verifyToken } from '../middleware/VerifyToken.js'
 
 const router = express.Router()
 
-// users
-router.get('/users', getUsers) // done
-router.get('/me', verifyToken, Me) // done
-router.post('/users', Register) // done
-router.post('/login', Login) // done
-router.post('/logout', verifyToken, Logout) // done
-router.put('/update/:id', verifyToken, updateUsersWithToken) // done
-router.delete('/delete', verifyToken, deleteUser)
+router.get('/users', getUsers)
+router.post('/register', Register)
+router.post('/login', Login)
+router.get('/me', Me)
+router.post('/logout', Logout)
+router.put('/user/update', updateUsersWithToken)
+router.put('/user/password', updatepassword)
+router.delete('/user/delete', deleteUser)
 
 export default router
